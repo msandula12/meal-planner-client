6402bfd03e5e2587df08bb909d3b08116e71dfb2
Mike Sandula <msandula@gmail.com>
2021-05-03 09:48:28 -0400

    fix: background-color styling

--- src/components/MealPlanner/styles.js
+++ src/components/MealPlanner/styles.js
@@ -3,7 +3,5 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
   root: {
     padding: '2rem',
-    backgroundColor: 'turquoise',
-    flexGrow: 1,
   },
 }));
