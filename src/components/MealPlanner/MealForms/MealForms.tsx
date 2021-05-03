1f642adca345df7b4264ade84323bbacdc126886
Mike Sandula <msandula@gmail.com>
2021-05-03 13:23:45 -0400

    feat: add close menu button

--- src/components/MealPlanner/MealForms/MealForms.tsx
+++ src/components/MealPlanner/MealForms/MealForms.tsx
@@ -1,21 +1,35 @@
 import { FC, Fragment } from 'react';
 import moment from 'moment';
-import { Divider, Drawer, Typography } from '@material-ui/core';
+import { Divider, Drawer, IconButton, Typography } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 import MealForm from './MealForm/MealForm';
 
 import { Day } from '../../../constants/interfaces';
+
+import useStyles from './styles';
 
 type Props = {
   day: Day;
 }
 
 const MealForms: FC<Props> = ({ day }) => {
+  const classes = useStyles();
+
+  const closeMealForms = () => {
+    console.log('Close <MealForms />');
+  }
+
   return (
     <Drawer anchor='right' variant='persistent' open>
-      <Typography variant='subtitle1' gutterBottom>
-        {moment(day.day).format('dddd, MMMM D')}
-      </Typography>
+      <div className={classes.drawerHeader}>
+        <Typography variant='subtitle1'>
+          {moment(day.day).format('dddd, MMMM D')}
+        </Typography>
+        <IconButton aria-label='Close form' color='secondary' onClick={closeMealForms}>
+          <CloseIcon />
+        </IconButton>
+      </div>
       {day.meals.map((meal, index) => (
         <Fragment key={meal.type}>
           <MealForm meal={meal} /> 
---
+++ src/components/MealPlanner/MealForms/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  drawerHeader: {
+    padding: '1rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+}));
