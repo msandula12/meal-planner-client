00d461ec4e42dfb725baf12ed0d10a80fabbdef5
Mike Sandula <msandula@gmail.com>
2021-06-22 14:48:10 -0400

    chore: change date format

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 
 export const formatDate = (date: string) => {
-  return isToday(date) ? 'Today' : dayjs(date).format('MMM DD');
+  return isToday(date) ? 'Today' : dayjs(date).format('MMM D');
 };
 
 export const isToday = (date: string) => dayjs().isSame(date, 'day');
