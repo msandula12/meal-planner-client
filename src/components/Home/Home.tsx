23914ab141452207704b7afb0d71b3cfe5e4c1bb
Mike Sandula <msandula@gmail.com>
2021-06-23 07:39:11 -0400

    feat: add footer to landing page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 
 import DashboardImage from 'assets/images/dashboard.png';
 import { Routes } from 'constants/enums';
@@ -18,24 +19,29 @@ function Home() {
 
   return (
     <div className="home">
-      <div className="home-hero">
-        <div>
-          <Link to={Routes.HOME} className="logo big">
-            MealPlanner
-          </Link>
+      <main className="home-above-the-fold">
+        <div className="home-hero">
+          <div>
+            <Link to={Routes.HOME} className="logo big">
+              MealPlanner
+            </Link>
+          </div>
+          <p>Your own personal meal calendar. No dry erase markers required.</p>
+          <div className="hero-image">
+            <img src={DashboardImage} alt="MealPlanner Dashboard" />
+          </div>
         </div>
-        <p>Your own personal meal calendar. No dry erase markers required.</p>
-        <div className="hero-image">
-          <img src={DashboardImage} alt="MealPlanner Dashboard" />
+        <div className="home-form">
+          {isSigningUp ? (
+            <SignUp toggle={toggleIsSigningUp} />
+          ) : (
+            <LogIn toggle={toggleIsSigningUp} />
+          )}
         </div>
-      </div>
-      <div className="home-form">
-        {isSigningUp ? (
-          <SignUp toggle={toggleIsSigningUp} />
-        ) : (
-          <LogIn toggle={toggleIsSigningUp} />
-        )}
-      </div>
+      </main>
+      <footer className="home-footer">
+        Copyright &copy;{dayjs().year()} Mike Sandula
+      </footer>
     </div>
   );
 }
