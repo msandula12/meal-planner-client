b711a238fdf41eb60eac8fbd41b5a65441564628
Mike Sandula <msandula@gmail.com>
2021-06-29 13:49:48 -0400

    feat: add useDarkMode to state

--- src/redux/reducers/userSlice.ts
+++ src/redux/reducers/userSlice.ts
@@ -5,10 +5,12 @@ import { User } from 'constants/interfaces';
 import type { RootState } from '../store';
 
 interface UserState {
+  useDarkMode: boolean;
   user: User | null;
 }
 
 const initialState: UserState = {
+  useDarkMode: false,
   user: null,
 };
 
@@ -17,6 +19,9 @@ export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    changeDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.useDarkMode = action.payload;
+    },
     changeUser: (state, action: PayloadAction<User | null>) => {
       state.user = action.payload;
     },
@@ -24,10 +29,11 @@ export const userSlice = createSlice({
 });
 
 // Actions
-export const { changeUser } = userSlice.actions;
+export const { changeDarkMode, changeUser } = userSlice.actions;
 
 // Selectors
 export const selectCurrentUser = (state: RootState) => state.user.user;
+export const selectIsDarkMode = (state: RootState) => state.user.useDarkMode;
 
 // Export reducer
 export default userSlice.reducer;
