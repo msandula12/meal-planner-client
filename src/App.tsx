ad0dfd740a21828b7a8dab7ee56a1afcc7f53aec
Mike Sandula <msandula@gmail.com>
2021-06-30 08:11:55 -0400

    feat: dark mode colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch, useHistory } from 'react-router-dom';
+import classNames from 'classnames';
 
 import { getUserFromToken, getUserToken, isValidToken } from 'api/users';
 import { Routes } from 'constants/enums';
-import { changeUser } from 'redux/reducers/userSlice';
+import { changeUser, selectIsDarkMode } from 'redux/reducers/userSlice';
 
 import Home from 'components/Home/Home';
 import MealPlanner from 'components/MealPlanner/MealPlanner';
@@ -14,6 +15,7 @@ import ProtectedRoute from 'components/ProtectedRoute/ProtectedRoute';
 function App() {
   const dispatch = useDispatch();
   const history = useHistory();
+  const isDarkMode = useSelector(selectIsDarkMode);
 
   useEffect(() => {
     const token = getUserToken();
@@ -25,15 +27,19 @@ function App() {
     }
   }, [dispatch, history]);
 
+  const cls = classNames({
+    dark: isDarkMode,
+  });
+
   return (
-    <>
+    <div className={cls}>
       <Switch>
         <Route exact path={Routes.HOME} component={Home} />
         <Route path={Routes.DEMO} component={MealPlanner} />
         <ProtectedRoute path={Routes.SCHEDULE} component={MealPlanner} />
         <Route path={Routes.NOT_FOUND} component={NotFound} />
       </Switch>
-    </>
+    </div>
   );
 }
 
