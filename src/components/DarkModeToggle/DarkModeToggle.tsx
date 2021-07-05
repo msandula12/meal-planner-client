3cad4e7ad379a8e6eb08ad77f8f17508d5922b54
Mike Sandula <msandula@gmail.com>
2021-07-05 14:10:00 -0400

    feat: dark mode toggle switch

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -3,21 +3,34 @@ import { BiMoon, BiSun } from 'react-icons/bi';
 
 import { ThemeContext } from 'context/ThemeContext';
 
+import './DarkModeToggle.scss';
+
 function DarkModeToggle() {
   const { isDarkMode, toggleDarkMode } = useContext(ThemeContext);
 
-  return isDarkMode ? (
-    <BiMoon
-      className="icon"
-      onClick={toggleDarkMode}
-      title="Change to light mode"
-    />
-  ) : (
-    <BiSun
-      className="icon"
-      onClick={toggleDarkMode}
-      title="Change to dark mode"
-    />
+  // return isDarkMode ? (
+  //   <BiMoon
+  //     className="icon"
+  //     onClick={toggleDarkMode}
+  //     title="Change to light mode"
+  //   />
+  // ) : (
+  //   <BiSun
+  //     className="icon"
+  //     onClick={toggleDarkMode}
+  //     title="Change to dark mode"
+  //   />
+  // );
+  return (
+    <div
+      className="theme-switch-wrapper"
+      title={`Change to ${isDarkMode ? 'light' : 'dark'} mode`}
+    >
+      <label className="theme-switch" htmlFor="checkbox">
+        <input type="checkbox" id="checkbox" onChange={toggleDarkMode} />
+        <div className="slider round"></div>
+      </label>
+    </div>
   );
 }
 
